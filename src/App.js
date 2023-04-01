a27d8673a6a8d95c8bb8627e026db6875b157f97
Geoff Holden <geoff@brightloudnoise.com>
2023-04-01 00:45:12 -0400

    repro - https://github.com/bvaughn/react-resizable-panels/issues/82

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PanelGroup direction="horizontal">
+        <Panel>
+          <div>Panel 1</div>
+        </Panel>
+        <PanelResizeHandle />
+        <Panel>
+          <div>Panel 2</div>
+        </Panel>
+      </PanelGroup>
+     
     </div>
   );
 }
